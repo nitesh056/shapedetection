9862840eaa65fa37c751e890eb0bdeacd4c3746d
nitesh056 <52165638+nitesh056@users.noreply.github.com>
2020-05-02 18:15:08 +0545

    another changes

--- camera.py
+++ camera.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class VideoCamera(object):
     def __init__(self):
-       self.video = cv2.VideoCapture(0)
+       self.video = cv2.VideoCapture(1)
        self.shape = "No shape detected"
 
     def __del__(self):
@@ -14,10 +14,12 @@ class VideoCamera(object):
     def get_frame(self):
         _, frame = self.video.read()
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        x, y = 300, 250
-        
-        # cv2.rectangle(frame, (x, y), (x, y), (0, 0, 0), 2)
-        px = frame[x,y]
+        height = frame.shape[0]
+        width = frame.shape[1]
+        h_height = int(height / 2)
+        h_width = int(width / 2)
+        cv2.rectangle(frame, (h_width+2, h_height+2), (h_width+2, h_height+2), (255, 0, 0), 2)
+        px = frame[h_height, h_width]
         px_array = np.uint8([[px]])
         px_hsv = cv2.cvtColor(px_array, cv2.COLOR_BGR2HSV)
 
@@ -26,7 +28,7 @@ class VideoCamera(object):
         min_v = (px_hsv[0][0][2]-20 if px_hsv[0][0][2]-20 > 0 else 0)
         max_h = (px_hsv[0][0][0]+20 if px_hsv[0][0][0]+20 < 180 else 180)
         max_s = (px_hsv[0][0][1]+20 if px_hsv[0][0][1]+20 < 255 else 255)
-        max_v = (px_hsv[0][0][2]+40 if px_hsv[0][0][2]+20 < 255 else 255)
+        max_v = (px_hsv[0][0][2]+20 if px_hsv[0][0][2]+20 < 255 else 255)
 
         lower = np.array([min_h, min_s, min_v])
         upper = np.array([max_h, max_s, max_v])
@@ -45,8 +47,12 @@ class VideoCamera(object):
                 cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
                 if len(approx) == 3:
                     self.shape = "Triangle"
-                elif 3 < len(approx) >= 10:
+                elif len(approx) == 4:
                     self.shape = "Rectangle"
+                elif len(approx) == 5:
+                    self.shape = "Pentagon"
+                elif len(approx) == 6:
+                    self.shape = "Hexagon"
                 elif 10 < len(approx) < 20:
                     self.shape = "Circle"
 
